7df6aa8155f657f306d56204971cad847ac13a36
SherazAhmad01 <sherazshabbir0300@gmail.com>
2022-08-29 10:54:10 +0500

    real estate

---
+++ realestatefrontend/realestate/src/component/Footer.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import './Footer.css'
+export default function Footer() {
+  
+  return (
+    <div>
+
+
+<footer>
+  <div className="main">
+    <div className="col1">
+      <h3 className="heading">
+        CATEGORIES
+      </h3>
+      <ul>
+        <li>
+          <a href="/asdasda">
+            Houses
+          </a>
+        </li>
+        <li>
+          <a href="/asdasda">
+            Shops
+          </a>
+        </li>
+        <li>
+          <a href="/asdasda">
+            Shopping Plaza
+          </a>
+        </li>
+    
+      </ul>
+    </div>
+
+    <div className="col2">
+      <h3 className="heading">
+        CITIES
+      </h3>
+      <div className="languages">
+        <a href="/asdasda">faisalabad</a>
+        <a href="/asdasda">Islamabad</a>
+        <a href="/asdasda">Karachi</a>
+        
+      </div>
+    </div>
+
+    <div className="col3">
+      <h3 className="heading">
+        Get in touch
+      </h3>
+      <div className="social">
+        <a href="/asdasda">
+          <ion-icon name="logo-facebook"></ion-icon>
+        </a>
+        <a href="/asdasda">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a>
+        <a href="/asdasda">
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <p className="terms">
+    <a href="/asdasda">Terms of purchase</a>
+    <a href="/asdasda">Security and privacy</a>
+    <a href="/asdasda">Newsletter</a>
+  </p>
+</footer>
+
+
+    </div>
+  )
+}
